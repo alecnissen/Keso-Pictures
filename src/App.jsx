4c59017be43ba5ffa549f24e9731d230cedaf186
alecnissen <alecnissen@yahoo.com>
2024-07-31 16:42:10 -0400

    added welcome title and subtitle text, saving work and will add the two images along with package/service options next to the images

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Styles/index.scss';
 import Header from './Components/Header';
 import MainPhotoHome from './Components/MainPhotoHome';
-import DescriptionAndPhotoHome from './Components/DescriptionAndPhotoHome';
+import WelcomeImg from './Components/WelcomeImg';
 
 export default function App() {
   return (
     <>
     <Header></Header>
-    <DescriptionAndPhotoHome></DescriptionAndPhotoHome> 
+    <WelcomeImg></WelcomeImg>
     </>
   );
 }
---
+++ src/Components/WelcomeImg.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import descriptionAndTextImage from '../assets/6883.jpg';
+
+export default function WelcomeImg() {
+  return (
+    <>
+      <div className="welcomeimg-content-wrapper">
+        <div className="welcome-title-subtitle-container">
+          <h1 className="welcome-header">Welcome</h1>
+
+          <p className="welcome-subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sed totam quaerat
+            expedita amet consequatur incidunt quo, iure dolorem. Soluta itaque necessitatibus quasi
+            veritatis impedit nemo at voluptate non molestiae? Quia, deleniti eos assumenda incidunt
+            dolorem at nostrum doloremque dolor perspiciatis, eius deleniti quaerat corrupti
+            expedita error iste unde nobis sequi.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
