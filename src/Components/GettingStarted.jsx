c3589a904c61600c174094ed9d10137a3fd58a55
alecnissen <alecnissen@yahoo.com>
2024-08-10 17:08:37 -0400

    made sure numbers line up in need help section

--- src/Components/GettingStarted.jsx
+++ src/Components/GettingStarted.jsx
@@ -5,17 +5,22 @@ export default function GettingStarted() {
     <> 
     <div className='getting-started-content-wrapper'>
       <h1 className='need-help-getting-started-text'>Need Help Getting Started?</h1>
-      <h1>1. Browse Our <a href='#'>Packages</a></h1>
-      <h1>2. Browse Our <a href='#'>Inventory</a></h1>
-      <h1>3. Create a <a href='#'>Wishlist & Submit</a></h1>
-
+      
+      
+      <div className='getting-started-links-container'>
+      
+      <div className='container-for-headers'>
+      <h1>1. Browse Our <a href='#' className='a-tag-styles'>Packages</a></h1>
+      <h1>2. Browse Our <a href='#' className='a-tag-styles'>Inventory</a></h1>
+      <h1>3. Create a <a href='#' className='a-tag-styles'>Wishlist & Submit</a></h1>
+      </div>
 
 
       <h1> 
-
       <button>About Us</button>
-
       </h1>
+
+      </div>
 
     </div>
     </>
