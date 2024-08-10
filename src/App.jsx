6e68857bd8fa39fc99563af946333216114a9766
alecnissen <alecnissen@yahoo.com>
2024-08-10 16:40:56 -0400

    updated media queries for img and package section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Styles/index.scss';
 import Header from './Components/Header';
 import PhotosAndLinks from './Components/PhotosAndLinks';
-
+import GettingStarted from './Components/GettingStarted';
 
 export default function App() {
   return (
     <>
     <Header></Header>
     <PhotosAndLinks></PhotosAndLinks>
+    <GettingStarted></GettingStarted>
     </>
   );
 }
---
+++ src/Components/GettingStarted.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function GettingStarted() {
+  return (
+    <> 
+    <div className='getting-started-content-wrapper'>
+      <h1 className='need-help-getting-started-text'>Need Help Getting Started?</h1>
+      <h1>1. Browse Our <a href='#'>Packages</a></h1>
+      <h1>2. Browse Our <a href='#'>Inventory</a></h1>
+      <h1>3. Create a <a href='#'>Wishlist & Submit</a></h1>
+
+
+
+      <h1> 
+
+      <button>About Us</button>
+
+      </h1>
+
+    </div>
+    </>
+  )
+}
