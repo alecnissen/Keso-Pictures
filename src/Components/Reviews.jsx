4e2cbfa9635320f86a45dfaf1451365d1005c77f
alecnissen <alecnissen@yahoo.com>
2024-08-11 00:29:31 -0400

    img carousel complete

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,44 +1,55 @@
-import React from 'react'
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import img1 from '../assets/2150191520.jpg';
-import img2 from '../assets/2150191521.jpg'
+import img2 from '../assets/2150191521.jpg';
 
 export default function Reviews() {
   return (
-    <> 
-    <div className='reviews-content-wrapper'>
- 
+    <>
+      <div className="reviews-content-wrapper">
+        <Carousel>
+          <Carousel.Item>
 
-    <Carousel>
-      <Carousel.Item>
-       <img src={img1}></img>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={img2}></img>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+            <div className="carousel-review-item">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dolores officia
+                facilis molestiae dignissimos, qui suscipit libero tenetur consectetur earum in,
+                officiis asperiores maiores non. Temporibus dolor ipsam suscipit magnam. Eveniet
+                fuga mollitia voluptatum repudiandae nam qui ducimus culpa labore laboriosam
+                aliquam. Illo, sed voluptate corrupti deserunt vero quisquam tenetur nesciunt atque
+                quo nobis officia eius placeat fugit? Quo, consequatur.
+              </p>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+          <div className="carousel-review-item">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dolores officia
+                facilis molestiae dignissimos, qui suscipit libero tenetur consectetur earum in,
+                officiis asperiores maiores non. Temporibus dolor ipsam suscipit magnam. Eveniet
+                fuga mollitia voluptatum repudiandae nam qui ducimus culpa labore laboriosam
+                aliquam. Illo, sed voluptate corrupti deserunt vero quisquam tenetur nesciunt atque
+                quo nobis officia eius placeat fugit? Quo, consequatur.
+              </p>
+            </div>
 
-
+          </Carousel.Item>
 
 
-    </div>
+          <Carousel.Item>
+             <div className="carousel-review-item">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus dolores officia
+                facilis molestiae dignissimos, qui suscipit libero tenetur consectetur earum in,
+                officiis asperiores maiores non. Temporibus dolor ipsam suscipit magnam. Eveniet
+                fuga mollitia voluptatum repudiandae nam qui ducimus culpa labore laboriosam
+                aliquam. Illo, sed voluptate corrupti deserunt vero quisquam tenetur nesciunt atque
+                quo nobis officia eius placeat fugit? Quo, consequatur.
+              </p>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
-  )
+  );
 }
