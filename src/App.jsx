bcca244c0e5c5391a271d6f78df353a6823b2f9a
alecnissen <alecnissen@yahoo.com>
2024-08-11 22:09:40 -0400

    added content for footer, saving work and will begin media queries

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PhotosAndLinks from './Components/PhotosAndLinks';
 import GettingStarted from './Components/GettingStarted';
 import Reviews from './Components/Reviews';
 import Gallery from './Components/Gallery';
+import Footer from './Components/Footer';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
     <GettingStarted></GettingStarted>
     <Reviews></Reviews>
     <Gallery></Gallery>
+    <Footer></Footer>
     </>
   );
 }
