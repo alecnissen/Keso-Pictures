6682ab02290728b301c2c784f5c5d72ff3ee8c19
alecnissen <alecnissen@yahoo.com>
2024-08-20 16:52:44 -0400

    added services header, with the two star svg icons next to it, saving work

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Header from './Header'
+import starImg from '../assets/star-alt-4-svgrepo-com.svg'
 
 export default function Services() {
   return (
-    <div>
-        <Header></Header>
-      <h1>HELLO WORLD</h1>
+    <> 
+    <Header></Header>
+    <div className='services-content-wrapper'>
+
+    <div className='service-options-container'> 
+
+
+    <div className='services-header-container'> 
+
+    <h1>Services</h1>
+
+    <img src={starImg} className='star-img-1'></img>
+
+    <img src={starImg} className='star-img-2'></img>
+
     </div>
+
+
+
+    </div>
+
+    
+
+    </div>
+    </>
   )
 }
