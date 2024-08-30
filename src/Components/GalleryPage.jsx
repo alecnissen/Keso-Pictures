e5c2b69fe26337bd4350430cc6b623c91acd6e6a
alecnissen <alecnissen@yahoo.com>
2024-08-30 15:34:34 -0400

    added videos, saving work and will do responsiveness once I get approval, moving onto to about section

--- src/Components/GalleryPage.jsx
+++ src/Components/GalleryPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import kesoPhotoboothImg from '../assets/20231006_175745 (1).jpg'
+import Footer from '../Components/Footer';
 import galleryImg1 from '../assets/1696634302_20.jpg';
 import galleryImg2 from '../assets/1696634598_26.jpg';
 import galleryImg3 from '../assets/1696636671_43.jpg'; 
@@ -13,6 +14,11 @@ import galleryImg9 from '../assets/DSC06020_1.jpg';
 import galleryImg10 from '../assets/DSC06024_1.jpg';
 import galleryImg11 from '../assets/DSC04781_1.jpg';
 import galleryImg12 from '../assets/DSC04856_1.jpg';
+import video1 from '../assets/video (1).mp4';
+import video2 from '../assets/video (3).mp4';
+import video3 from '../assets/video (6).mp4';
+import video4 from '../assets/video (14).mp4';
+
 
 export default function GalleryPage() {
   return (
@@ -23,7 +29,7 @@ export default function GalleryPage() {
         <h1>Customize Your Style</h1>
       </div>
 
-      <div className='photo-container'>
+      <div className='photo-container-gallery-page'>
         <img src={galleryImg1}></img>
         <img src={galleryImg2}></img>
         <img src={galleryImg3}></img>
@@ -36,10 +42,30 @@ export default function GalleryPage() {
         <img src={galleryImg10} className='colored-img'></img>
         <img src={galleryImg11} className='colored-img'></img>
         <img src={galleryImg12} className='colored-img'></img>
+
+        <video width="400" height="520" controls>
+        <source src={video1} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <video width="400" height="520" controls>
+        <source src={video2} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <video width="400" height="520" controls>
+        <source src={video3} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+    
+
+
       </div>
 
 
     </div>
+    <Footer></Footer>
     </>
   )
 }
