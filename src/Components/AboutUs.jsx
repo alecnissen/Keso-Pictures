21c594795fea32f3d23bb830873cfa7de452a221
alecnissen <alecnissen@yahoo.com>
2024-08-30 17:25:31 -0400

    completed about us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -3,37 +3,61 @@ import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import { Link } from 'react-router-dom';
 import kesoAboutUsImg from '../assets/keso-about-us.png';
+import aboutUsImg1 from '../assets/DSC04817_1.jpg';
+import aboutUsImg2 from '../assets/DSC04806_1.jpg';
 
 export default function AboutUs() {
   return (
-    <> 
-    <Header></Header>
+    <>
+      <Header></Header>
 
+      <div className="about-us-content-wrapper">
+        <div className="hello-container">
+          <h1>Hello There!</h1>
 
-    <div className='about-us-content-wrapper'> 
-
-        <div className='about-us-container'>
-            <h1>Hello There!</h1>
-
-
-            <div className='about-us-textbox'> 
-            
-            <p>We're Cheryl and Mike, the owners and operators of Keso Pictures. 
-                We are so happy you're here and look forward to chatting with you
-                about your special day! <Link>Send us a message </Link> 
-                 and let's get started making your day even more memorable!
+          <div className="hello-textbox">
+            <p>
+              We're Cheryl and Mike, the owners and operators of Keso Pictures. We are so happy
+              you're here and look forward to chatting with you about your special day!{' '}
+              <Link>Send us a message </Link>
+              and let's get started making your day even more memorable!
             </p>
+          </div>
 
-            </div>
+          <img src={kesoAboutUsImg}></img>
+        </div>
 
-            <img src={kesoAboutUsImg}></img>
+        <div className="about-us-container">
+          <h1>About Us</h1>
 
+          <div className="about-us-textbox">
+            <p>At <strong>Keso Pictures</strong></p>
+            <p>
+            Our mission is to help capture memories and spread joy through our fun 
+            and playful photo experiences. We believe that every event, big or small
+            , deserves to be celebrated in a unique and special way. Our photo booths provide 
+            a way for guests to let loose and have fun while creating lasting memories. 
+            Whether it's a wedding, corporate event, or birthday party, we're dedicated
+            to bringing smiles and laughter to every occasion, so strike a pose, grab a prop, 
+            and come smile with us!
+            </p>
+          </div>
         </div>
 
 
-    </div>
+        <div className='about-us-photo-container'> 
+        <img src={aboutUsImg1}></img>
+        <img src={aboutUsImg2}></img>
+        </div>
 
-    <Footer></Footer>
+
+
+
+
+
+      </div>
+
+      <Footer></Footer>
     </>
-  )
+  );
 }
